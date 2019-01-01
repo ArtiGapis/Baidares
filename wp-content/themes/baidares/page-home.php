57d7632f3644ec4edd10f976824048f065d98e86
Arturas <arturas.danilevicius@gmail.com>
2019-01-01 21:12:43 +0200

    baidares project header

---
+++ wp-content/themes/baidares/page-home.php
@@ -0,0 +1,22 @@
+<?php /* Template Name: Homepage */
+get_header(); ?>
+<?php /*page-{slug}.php
+page-{ID}.php */ ?>
+
+<?php  get_page_template()?>
+<?php if(have_posts()) : while(have_posts()) : the_post(); ?>
+
+    <div class="container home-page">
+        <div class="row">
+            <div class="col-lg-2"></div>
+            <div class="col-lg-8 container-white">
+                <div class="rte">
+                    <?php the_content(); ?>
+                </div>
+            </div>
+            <div class="col-lg-2"></div>
+        </div>
+    </div>
+<?php endwhile; endif; ?>
+
+<?php get_footer(); ?>
---
+++ wp-content/themes/baidares/page-post.php
@@ -0,0 +1,65 @@
+<?php /*
+Template Name: post
+Description: A Page Template for custom page.
+*/
+get_header(); ?>
+<?php if(have_posts()) : while(have_posts()) : the_post(); ?>
+
+
+    <div id="main-content" class="visual-container">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-2">
+                    <div class="left-wrap">
+						<?php get_sidebar(); ?>
+                    </div>
+                </div>
+                <div class="col-md-8 container-white">
+                    <div class="rte">
+                        <div id="wrap-page">
+                            <div class="post-wrap">
+                                <div class="forum-wrap">
+	                                <?php
+	                                global $post;
+	                                $args = array( 'posts_per_page' => 3 );
+	                                $lastposts = get_posts( $args );
+	                                foreach ( $lastposts as $post ) :
+		                                setup_postdata( $post ); ?>
+                                    <div class="news-wraper">
+                                        <h2><?php the_title(); ?></h2>
+                                        <a href="<?php the_permalink(); ?>"> Plačiau>>>> </a>
+                                    </div>
+	                                <?php endforeach;
+	                                wp_reset_postdata(); ?>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-sm-2">
+                    <div >
+			            <?php
+			            $i = 0;
+			            $args = array( 'post_type' => 'right-menu', 'posts_per_page' => 5 );
+			            $loop = new WP_Query( $args );
+			            while ( $loop->have_posts() ) : $loop->the_post(); ?>
+                            <div class="right-menu-wraper">
+                                <a href="<?php the_permalink(); ?>">
+                                    <div class="menu-foto">
+							            <?php the_post_thumbnail(); ?>
+                                    </div>
+                                    <div class="menu-title">
+							            <?php the_title(); ?>
+                                    </div>
+                                </a>
+                            </div>
+				            <?php $i++; endwhile; wp_reset_postdata(); ?>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </div>
+<?php endwhile; endif; ?>
+
+<?php get_footer(); ?>
